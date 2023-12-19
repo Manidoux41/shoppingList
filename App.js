3486888cb44203f02310ead948dd6d902589c936
Manidoux41 <lherissondu41@gmail.com>
2023-12-19 22:27:30 +0100

    first commit app

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { FlatList, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native'
 
-export default function App() {
+const App = () => {
+
+  const [product, setproduct] = useState('');
+  const [myProducts, setMyProducts] = useState([])
+
+  const inputHandler = (val) => {
+    setproduct(val)
+  }
+
+  const submitHandler = () => {
+    const idString = Date.now().toString();
+    setMyProducts(currentMyProducts => [{key: idString, name: product}, ...currentMyProducts])
+    setproduct('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Nouveau produit'
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button
+          title='Valider'
+          onPress={submitHandler}
+        />
+      </View>
+
+      <FlatList 
+        data={myProducts}
+        renderItem={({item}) => <Text
+          style={styles.element}
+          >
+            {item.name}
+            </Text> }
+      />
+      
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+    paddingTop: 60,
   },
-});
+  inputContainer: {
+    flexDirection: "row",
+    gap:5,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 5,
+    paddingLeft: 9,
+    fontSize: 18,
+    flexGrow: 1,
+  },
+  items: {
+    marginTop: 10,
+    },
+  element: {
+    backgroundColor: "#ffb6c1",
+    padding:20,
+    fontSize: 17,
+    marginVertical: 6
+    
+  }
+})
